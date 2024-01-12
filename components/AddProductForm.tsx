d19092124f62ef7e157080b8d307d9a5102c0591
Yulya <iuliiaobr.io@gmail.com>
2024-01-12 19:57:20 +0700

    fix prettier

--- components/AddProductForm.tsx
+++ components/AddProductForm.tsx
@@ -1,19 +1,24 @@
-import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
+import { PhotoIcon } from "@heroicons/react/24/solid";
 
 function AddProductForm() {
   return (
     <form>
       <div className="space-y-12">
         <div className="pb-12">
-          <h2 className="text-4xl font-semibold leading-7 text-gray-900">Add a new product</h2>
+          <h2 className="text-4xl font-semibold leading-7 text-gray-900">
+            Add a new product
+          </h2>
           <p className="mt-4 text-sm leading-6 text-gray-600">
             Please enter product&apos;s title, description, image, price and etc
           </p>
 
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-4">
-              <label htmlFor="productname" className="block text-sm font-medium leading-6 text-gray-900">
-                Product
+              <label
+                htmlFor="productname"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Product name
               </label>
               <div className="mt-2">
                 <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
@@ -27,18 +32,21 @@ function AddProductForm() {
                   />
                 </div>
               </div>
-            </div> 
+            </div>
 
             <div className="sm:col-span-4">
-              <label htmlFor="productprice" className="block text-sm font-medium leading-6 text-gray-900">
-                Product
+              <label
+                htmlFor="productprice"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Product price
               </label>
               <div className="mt-2">
                 <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                   <input
                     type="number"
-                    min='1'
-                    step='any'
+                    min="1"
+                    step="any"
                     name="productprice"
                     id="productprice"
                     className="block flex-1 border-0 bg-transparent py-1.5 pl-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
@@ -49,7 +57,10 @@ function AddProductForm() {
             </div>
 
             <div className="col-span-full">
-              <label htmlFor="productdescription" className="block text-sm font-medium leading-6 text-gray-900">
+              <label
+                htmlFor="productdescription"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
                 Product description
               </label>
               <div className="mt-2">
@@ -58,31 +69,44 @@ function AddProductForm() {
                   name="productdescription"
                   rows={3}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  defaultValue={''}
-                  placeholder='Please enter the product description here'
+                  defaultValue={""}
+                  placeholder="Please enter the product description here"
                 />
               </div>
               {/* <p className="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about yourself.</p> */}
             </div>
 
             <div className="col-span-full">
-              <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">
-                Product&apos;s photo
+              <label
+                htmlFor="cover-photo"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Product image
               </label>
               <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
                 <div className="text-center">
-                  <PhotoIcon className="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
+                  <PhotoIcon
+                    className="mx-auto h-12 w-12 text-gray-300"
+                    aria-hidden="true"
+                  />
                   <div className="mt-4 flex text-sm leading-6 text-gray-600">
                     <label
                       htmlFor="file-upload"
                       className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
                     >
                       <span>Upload a file</span>
-                      <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+                      <input
+                        id="file-upload"
+                        name="file-upload"
+                        type="file"
+                        className="sr-only"
+                      />
                     </label>
                     <p className="pl-1">or drag and drop</p>
                   </div>
-                  <p className="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
+                  <p className="text-xs leading-5 text-gray-600">
+                    PNG, JPG, GIF up to 10MB
+                  </p>
                 </div>
               </div>
             </div>
@@ -91,7 +115,10 @@ function AddProductForm() {
       </div>
 
       <div className="mt-6 flex items-center justify-end gap-x-6">
-        <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
+        <button
+          type="button"
+          className="text-sm font-semibold leading-6 text-gray-900"
+        >
           Cancel
         </button>
         <button
@@ -102,8 +129,7 @@ function AddProductForm() {
         </button>
       </div>
     </form>
-  )
+  );
 }
-
 
 export default AddProductForm;
