98640d989c3487c32bbc06ec42027361e119183d
Yulya <iuliiaobr.io@gmail.com>
2024-01-15 18:44:15 +0700

    adds new badge to the recent added products

--- components/ProductCardPreview.tsx
+++ components/ProductCardPreview.tsx
@@ -8,6 +8,7 @@ type ProductCardPreviewProps = {
 };
 
 function ProductCardPreview({ product }: ProductCardPreviewProps) {
+    const isNew = Date.now() - new Date(product.createdAt).getTime() < (1000 * 60 * 60 * 24 *7)
   return (
     <Link
       className="card w-full bg-rose-50 transition-shadow hover:shadow-xl"
@@ -17,7 +18,10 @@ function ProductCardPreview({ product }: ProductCardPreviewProps) {
             <Image src={'/' + product.imageUrl} className="h-48 object-cover" alt={product.name} width={800} height={400}/>
         </figure>
       <div className="card-body">
-        <h2 className="card-title">{product.name}</h2>
+        <h2 className="card-title">
+            {product.name}
+            </h2>
+            {isNew && <div className="badge badge-secondary">NEW</div>}
         <p>{product.description}</p>
         <PriceTag price={product.price}/>
       </div>
