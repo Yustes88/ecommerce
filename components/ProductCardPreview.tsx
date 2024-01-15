281d96fa8514e30ff70a4ec862cb0c3861f64b8f
Yulya <iuliiaobr.io@gmail.com>
2024-01-15 18:37:35 +0700

    adds image figure without displaying it

--- components/ProductCardPreview.tsx
+++ components/ProductCardPreview.tsx
@@ -1,6 +1,7 @@
 import { Product } from "@prisma/client";
 import Link from "next/link";
 import PriceTag from "./PriceTag";
+import Image from "next/image";
 
 type ProductCardPreviewProps = {
   product: Product;
@@ -12,6 +13,9 @@ function ProductCardPreview({ product }: ProductCardPreviewProps) {
       className="card w-full bg-rose-50 transition-shadow hover:shadow-xl"
       href={`/products/${product.id}`}
     >
+        <figure>
+            <Image src={'/' + product.imageUrl} className="h-48 object-cover" alt={product.name} width={800} height={400}/>
+        </figure>
       <div className="card-body">
         <h2 className="card-title">{product.name}</h2>
         <p>{product.description}</p>
