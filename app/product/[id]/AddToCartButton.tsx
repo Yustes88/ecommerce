8fe268529cd8e761b54a3753146ca046673ba2d2
Yulya <iuliiaobr.io@gmail.com>
2024-01-16 19:10:46 +0700

    adds product to the database cart

--- app/product/[id]/AddToCartButton.tsx
+++ app/product/[id]/AddToCartButton.tsx
@@ -1,13 +1,27 @@
 'use client'
 
+import { useState, useTransition } from "react"
+
 type AddToCartButtonProps = {
     productId: string,
+    incrementProductQuantity: (productId: string) => Promise<void>
 }
 
-function AddToCartButton({productId}: AddToCartButtonProps) {
+function AddToCartButton({productId, incrementProductQuantity}: AddToCartButtonProps) {
+    const [isPending, startTransition] = useTransition();
+    const [success, setSuccess] = useState(false);
+
     return(
         <div className="flex items-center gap-2 ">
-            <button className="btn btn-primary" onClick={() => {}}>Add to Cart</button>
+            <button className="btn btn-primary" onClick={() => {
+                setSuccess(false)
+                startTransition(async () => {
+                    await incrementProductQuantity(productId);
+                    setSuccess(true)
+                })
+            }}>Add to Cart</button>
+            {isPending && <span className="loading loading-spinner loading-sm"/>}
+            {!isPending && success && <span className="text-success">Added to cart</span>}
         </div>
     )
 }
