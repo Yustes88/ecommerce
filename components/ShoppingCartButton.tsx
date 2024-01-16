1878c5288a297282e10fcbd9c9eca749bfb63e92
Yulya <iuliiaobr.io@gmail.com>
2024-01-16 20:36:30 +0700

    adds cart dropdown with size cart size and subtotal

--- components/ShoppingCartButton.tsx
+++ components/ShoppingCartButton.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ShoppingCart } from "@/lib/db/cart";
+import formatPrice from "@/lib/db/format";
 
 type ShoppingCartButtonProps = {
   cart: ShoppingCart | null;
@@ -28,6 +29,14 @@ function ShoppingCartButton({cart}: ShoppingCartButtonProps) {
           <span className="badge badge-sm indicator-item">{cart?.size || 0}</span>
         </div>
       </label>
+      <div tabIndex={0} className="card dropdown-content card-compact mt-3 w-52 bg-base-100 shadow z-30">
+        <div className="card-body">
+          <span className="text-lg font-bold">{cart?.size || 0} Items</span>
+          <span className="text-info">
+            Subtotal: {formatPrice(cart?.subtotal || 0)}
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
