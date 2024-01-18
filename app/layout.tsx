5995fa69c37ebe05744b8229c358ddbb52bf3afd
Yulya <iuliiaobr.io@gmail.com>
2024-01-18 20:33:30 +0700

    sets up nextauth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,36 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import clsx from 'clsx'
-import NavBar from '@/components/NavBar'
-import Footer from '@/components/Footer'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import clsx from "clsx";
+import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer";
+import SessionProvider from "./SessionProvider";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Ecommerce',
-  description: 'Waste your money here',
-}
+  title: "Ecommerce",
+  description: "Waste your money here",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={clsx(`${inter.className}, bg-white flex flex-col min-h-screen`)}>
-        <NavBar/>
-        <main className='p-4 m-auto max-w-3xl flex-grow '>
-          {children}
-        </main>
-        <Footer/>
-        </body>
+      <body
+        className={clsx(
+          `${inter.className}, flex min-h-screen flex-col bg-white`,
+        )}
+      >
+        <SessionProvider>
+          <NavBar />
+          <main className="m-auto max-w-3xl flex-grow p-4 ">{children}</main>
+          <Footer />
+        </SessionProvider>
+      </body>
     </html>
-  )
+  );
 }
