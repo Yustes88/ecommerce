ac6f88204090997bc53fe7ed1c2950356bfd0502
Yulya <iuliiaobr.io@gmail.com>
2024-01-19 20:38:15 +0700

    moves providers directly into handler

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,33 @@
-import { authOptions } from "@/lib/authUtils";
+import { mergeAnonymesCartIntoUserCart } from "@/lib/db/cart";
+import { env } from "@/lib/env";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import { Adapter } from "next-auth/adapters";
 import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
 
 
-const handler = NextAuth(authOptions);
+
+const handler = NextAuth({
+    adapter: PrismaAdapter(prisma as PrismaClient) as Adapter, 
+    providers: [
+        GoogleProvider({
+            clientId: env.GOOGLE_CLIENT_ID,
+            clientSecret: env.GOOGLE_CLIENT_SECRET
+        })
+    ],
+    callbacks: {
+        session({session, user}) {
+            session.user.id = user.id;
+            return session;
+        }
+    },
+
+    events: {
+        async signIn({user}) {
+            await mergeAnonymesCartIntoUserCart(user.id)
+        }
+    }
+});
 
 export { handler as GET, handler as POST }
