753c7276c1f878bdab55749e363f350e91102d48
Yulya <iuliiaobr.io@gmail.com>
2024-01-19 17:08:27 +0700

    adds pagination for the small screens

--- components/PaginationBar.tsx
+++ components/PaginationBar.tsx
@@ -20,9 +20,26 @@ function PaginationBar({currentPage, totalPages}: PaginationBarProps) {
     }
 
     return(
-        <div className="join">
+        <>
+        <div className="join hidden sm:block">
             {numberedPageItems}
         </div>
+        <div className="join block sm:hidden">
+        {currentPage > 1 && (
+          <Link href={"?page=" + (currentPage - 1)} className="join-item btn">
+            «
+          </Link>
+        )}
+        <button className="join-item btn pointer-events-none">
+          Page {currentPage}
+        </button>
+        {currentPage < totalPages && (
+          <Link href={"?page=" + (currentPage + 1)} className="join-item btn">
+            »
+          </Link>
+        )}
+      </div>
+        </>
     )
 }
 
