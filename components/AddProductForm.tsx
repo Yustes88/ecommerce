8a8c1497116ed3b2741ba2fcbe77e347e4852443
Yustes88 <iuliiaobr.io@gmail.com>
2024-01-21 14:19:51 +0300

    moves authOptions to the separate file, fix types

--- components/AddProductForm.tsx
+++ components/AddProductForm.tsx
@@ -2,7 +2,7 @@ import {prisma} from "@/lib/db/prisma";
 import { redirect } from "next/navigation";
 import Button from "./Button";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/utils/authOptions";
 
 async function addProduct(formData: FormData) {
   "use server";
