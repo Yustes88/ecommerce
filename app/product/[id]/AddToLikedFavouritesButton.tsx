8cecb54da7ad46390240c7758724bb39f9c69c27
Yulya <iuliiaobr.io@gmail.com>
2024-01-24 19:59:32 +0700

    adds liked button component

---
+++ app/product/[id]/AddToLikedFavouritesButton.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { HeartIcon } from "@heroicons/react/24/solid";
+import { HeartIcon as OutlinedHeart } from "@heroicons/react/24/outline";
+import { useState, useTransition } from "react";
+
+type AddToFavouritesButtonProps = {
+  productId: string;
+  incrementProductQuantity: (productId: string) => Promise<void>;
+  className?: string;
+};
+
+function AddToFavouritesButton({
+  productId,
+  incrementProductQuantity,
+  className,
+}: AddToFavouritesButtonProps) {
+  const [isLiked, setIsLiked] = useState(false);
+  const [isPending, startTransition] = useTransition();
+  const [success, setSuccess] = useState(false);
+
+  const Icon = isLiked ? HeartIcon : OutlinedHeart;
+
+  return (
+    <div className="flex justify-end items-end mt-2">
+      <button
+        className="transition hover:opacity-75"
+        onClick={() => {
+          if (isLiked) {
+            setIsLiked(false);
+          } else {
+            setIsLiked(true);
+          }
+
+          setSuccess(false);
+          startTransition(async () => {
+            await incrementProductQuantity(productId);
+            setSuccess(true);
+          });
+        }}
+      >
+        <Icon color={isLiked ? "red" : "red"} className="w-10" />
+      </button>
+      {isPending && <span className="loading loading-spinner loading-sm" />}
+      {!isPending && success && (
+        <span className="text-success">Added to cart</span>
+      )}
+    </div>
+  );
+}
+
+export default AddToFavouritesButton;
