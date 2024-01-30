344ea52e898764b8ee26d815e6f2b6562ea31b96
Yulya <iuliiaobr.io@gmail.com>
2024-01-30 15:40:19 +0700

    replace fav dropwdown with a simple link

--- components/FavouritesButton.tsx
+++ components/FavouritesButton.tsx
@@ -19,25 +19,14 @@ function FavouritesButton({favourites}: FavouritesButtonProps) {
   } 
 
   return (
-    <div className="dropdown dropdown-end">
+    <Link href='/favourite' className="">
       <label tabIndex={0} className="btn btn-circle btn-ghost">
         <div className="indicator">
             <HeartIcon className="w-8"/>
           <span className="badge badge-sm indicator-item">{favourites?.size || 0}</span>
         </div>
       </label>
-      <div tabIndex={0} className="card dropdown-content card-compact mt-3 w-52 bg-base-100 shadow z-30">
-        <div className="card-body">
-          <span className="text-info">
-            Favourites: {formatPrice(favourites?.subtotal || 0)}
-          </span>
-          <div className="card-actions">
-            <Link href='/favourite' className="btn btn-primary btn-block"
-            onClick={closeDropDown}>View favourites</Link>
-          </div>
-        </div>
-      </div>
-    </div>
+    </Link>
   );
 }
 
