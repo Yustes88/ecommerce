f6be17eb547bceb8e5bbeb80f734e65412fbf9dc
Yulya <iuliiaobr.io@gmail.com>
2024-02-01 16:57:39 +0700

    fix animation gap by adding items

--- components/Marquee.tsx
+++ components/Marquee.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const marquees = [
+  "Absolute Inspiration",
   "Sparkle & Shine",
   "Glittering Treasures",
   "Rings",
@@ -11,20 +12,30 @@ const marquees = [
   "Exquisite Adornments",
   "Earrings",
   "Impressive Details",
+  "Genuine Impression",
+  "Longlasting Effect",
 ];
 
 export const Marquee = () => {
   return (
     <div className="relative flex overflow-x-hidden text-sm">
       <div className="animate-marquee whitespace-nowrap py-5">
-        {marquees.map((marquee)=> {
-        return <span key={marquee} className="mx-2">{marquee}</span>
+        {marquees.map((marquee) => {
+          return (
+            <span key={marquee} className="mx-2">
+              {marquee}
+            </span>
+          );
         })}
       </div>
 
       <div className="animate-marquee2 absolute top-0 whitespace-nowrap py-5 text-sm">
-      {marquees.map((marquee)=> {
-        return <span key={marquee} className="mx-2">{marquee}</span>
+        {marquees.map((marquee) => {
+          return (
+            <span key={marquee} className="mx-2">
+              {marquee}
+            </span>
+          );
         })}
       </div>
     </div>
